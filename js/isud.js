78de4283f2ab0984f09e637d9893acb8a9e0282d
nanlar <46804475+nanlar@users.noreply.github.com>
2019-04-03 20:19:21 -0500

    Created isud javascript file for the ISUD site redesign.

---
+++ js/isud.js
@@ -0,0 +1,17 @@
+// jQuery 3.x-style ready event and locally scoped $
+jQuery(function($) {
+  $('html').removeClass('nojs');
+  $('html').addClass('hasjs');
+});
+
+// our functions go below
+
+  
+
+
+
+
+
+
+
+

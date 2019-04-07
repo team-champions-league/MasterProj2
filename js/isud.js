69bef784e32acd68439f6ea773680ba4c89f5d23
Rich05 <rich05@github>
2019-04-07 14:01:38 -0500

    Added JS
    I added JS for inputs and form valiration for registration form

--- js/isud.js
+++ js/isud.js
@@ -28,7 +28,35 @@ for (i = 0; i < acc.length; i++) {
 }
 
 
-  
+//submit and input for registration form//
+function validateForm() {
+  var x = document.forms["myForm"]["fname"].value;
+  if (x == "") {
+    alert("First name must be filled out");
+    return false;
+  }
+  var x = document.forms["myForm"]["lname"].value;
+  if (x == "") {
+    alert("Last name must be filled out");
+    return false;
+  }
+  var x = document.forms["myForm"]["email"].value;
+  if (x == "") {
+    alert("Email Address must be filled out");
+    return false;
+  }
+  var x = document.forms["myForm"]["email"].value;
+  var atpos = x.indexOf("@");
+  var dotpos = x.lastIndexOf(".");
+  if (atpos < 1 || dotpos < atpos + 2 || dotpos + 2 >= x.length) {
+    alert("Not a valid e-mail address");
+    return false;
+  } else {
+    alert("Thank you for registering!");
+    
+    }
+  }
+
 
 
 

49cc0a53578031e5e0ac366068c54607dc33a30a
Dani_Exodus <ddenekew@hawk.iit.edu>
2019-04-08 16:16:23 -0500

    added responsive for nav

--- js/isud.js
+++ js/isud.js
@@ -6,17 +6,27 @@ jQuery(function($) {
 
 // our team functions go below
 
-// acctivate the accordion 
+/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
+
+// acctivate the accordion
 var acc = document.getElementsByClassName("accordion");
 var i;
 
 for (i = 0; i < acc.length; i++) {
   acc[i].addEventListener("click", function() {
-    
+
     /* Toggle between adding and removing the "active" class,
     to highlight the button that controls the panel */
     this.classList.toggle("active");
-    
+
     /* Toggle between hiding and showing the active panel */
     var panel = this.nextElementSibling;
     if (panel.style.display === "block"){
@@ -53,15 +63,6 @@ function validateForm() {
     return false;
   } else {
     alert("Thank you for registering!");
-    
+
     }
   }
-
-
-
-
-
-
-
-
-
